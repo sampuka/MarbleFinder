35d2dbde9121e8e84a1f8f942462826031cebeaa
sampuka <matddi@gmail.com>
2018-12-07 11:06:52 +0100

    Updated fuzzytest.sh to reflect chosen tests

--- world/fuzzytest.sh
+++ world/fuzzytest.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
-worlds[0]=door.world
-worlds[1]=moon.world
-worlds[2]=cylender.world
+worlds[0]=moon.world
+worlds[1]=door.world
+worlds[2]=cylinder.world
 worlds[3]=horseshoe.world
+worlds[4]=corner.world
+worlds[5]=bluntangle.world
 
-dist=1
+dist=2
 
 for world in  ${worlds[*]}
 do
 	printf "   %s\n" $world
-	for i in -1.5 -0.5 0 0.5 1.5
+	for i in -1.57 -1.04 -0.79 -0.52 -0.17 0 0.17 0.52 0.79 1.04 1.57
 	do
 		xpos=$(echo "-$dist*c($i)" | bc -l)
 		ypos=$(echo "-$dist*s($i)" | bc -l)
@@ -18,13 +20,13 @@ do
 		line="      <pose>$xpos $ypos 0 0 0 $yaw<\\/pose>"
 		#echo $line
 		sed -i "37s/.*/$line/" $world
-		for j in 1 2 3
+		for j in 1 2 3 4 5 6 7 8 9 10
 		do
 			echo Test $j for angle $i for world $world
 			sh gazebo_server.sh $world &>/dev/null &
 			../build-robot_control-Desktop-Debug/robot_control &>/dev/null &
 			disown
-			sleep 10s
+			sleep 30s
 			killall -9 gzserver
 			killall -9 robot_control
 
